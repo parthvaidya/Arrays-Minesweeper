1947527649835c0e1b690732038868d477270c7f
Parth Vaidya <zeusparth99@gmail.com>
2024-11-24 11:31:58 +0530

    Mine count added

---
+++ Array-Minesweeper/source/Gameplay/GameplayService.cpp
@@ -0,0 +1,48 @@
+#include "../../header/Gameplay/GameplayService.h"
+
+namespace Gameplay
+{
+	GameplayService::GameplayService()
+	{
+		gameplay_controller = nullptr;
+		createController();
+	}
+
+	GameplayService::~GameplayService()
+	{
+		destroy();
+	}
+
+	void GameplayService::createController()
+	{
+		gameplay_controller = new GameplayController();
+	}
+
+	void GameplayService::initialize()
+	{
+		gameplay_controller->initialize();
+	}
+
+	void GameplayService::update()
+	{
+		gameplay_controller->update();
+	}
+
+	void GameplayService::render()
+	{
+		gameplay_controller->render();
+	}
+
+	void GameplayService::startGame()
+	{
+		gameplay_controller->restart();
+	}
+
+	
+
+	int GameplayService::getMinesCount()
+	{
+		return gameplay_controller->getMinesCount();
+	}
+
+}
